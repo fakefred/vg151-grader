cf9f29b4e9463773852d2f22e5409f40ebe1209b
Frederick Yin <fkfd@fkfd.me>
2022-10-14 13:41:18 +0800

    Update settings.example.py

--- settings.example.py
+++ settings.example.py
@@ -1,59 +1,45 @@
-CANVAS_TOKEN = "asdfjkl;"
-COURSE_ID = 1903
-SID = "asdfjl;"
+CANVAS_TOKEN = ""
+COURSE_ID = 41
 MOSS_USER_ID = 000000
-JOJ_COURSE_ID = "vg101_fall_2020_manuel"
+JOJ_COURSE_ID = "vg151_fall_2022_manuel"
+JOJ_SESSION_ID = ""
 GITEA_BASE_URL = "https://focs.ji.sjtu.edu.cn/git/api/v1"
-CANVAS_BASE_URL = "https://umjicanvas.com/api/v1"
-ORG_NAME = "vg101"
-PROCESS_COUNT = 16
+GITEA_TOKEN = ""
+CANVAS_BASE_URL = "https://jicanvas.com/api/v1"
+ORG_NAME = "ENGR151-22"
 RUBRIC = {
-    "indvFailSubmit": [-0.5, "individual submit missing"],
-    "indvUntidy": [-0.25, "individual branch untidy"],
-    "groupFailSubmit": [-2.5, "group submit missing"],
-    "groupUntidy": [-0.25, "group submit untidy"],
-    "jojFailHomework": [-0.5, "JOJ homework not passed"],
-    "jojFailExercise": [-0.25, "JOJ exercise not passed"],
-    "jojFailCompile": [-1, "JOJ fail to compile"],
-    "indvLowCodeQuality": [-0.25, "individual submit low code quality"],
-    "groupLowCodeQuality": [-0.25, "group submit low code quality"],
-    "noReview": [-1, "not review others' code"],
+    "indvFailSubmit": [-0.5, "Individual submission missing"],
+    "indvNoReadme": [-0.5, "Individual submission missing README"],
+    "indvUntidy": [-0.25, "Individual branch untidy"],
+    "groupFailSubmit": [-2.5, "Group submission missing"],
+    "groupNoReadme": [-0.5, "Group submission missing README"],
+    "groupUntidy": [-0.25, "Group submit untidy"],
+    "jojFailHomework": [-0.5, "JOJ: <50% test cases passed for homework"],
+    "jojFailExercise": [-0.25, "JOJ: <25% test cases passed for single exercise"],
+    "jojFailCompile": [-1, "JOJ: Fails to compile"],
+    "indvLowCodeQuality": [-0.25, "Low code quality in individual submission"],
+    "groupLowCodeQuality": [-0.25, "Low code quality in group submission"],
+    "noReview": [-1, "Did not review teammates' code"],
 }
+# h3
 JOJ_INFO = {
-    "lang": "c",
-    "homeworkID": "5f8533bb229b1b0006f11473",
+    "lang": "matlab",
+    "homeworkID": "6343c69eb6a0b300081b4d46",
     "problemInfo": [
-        [["ex1.c", "assignment.h"], "5f853289229b1b0006f1146a", 10],
-        [["ex2.c", "assignment.h"], "5f85330a229b1b0006f1146d", 10],
-        [
-            [
-                "ex5.c",
-                "prod.c",
-                "prod.h",
-                "sum.c",
-                "sum.h",
-                "exp.c",
-                "exp.h",
-                "quorem.c",
-                "quorem.h",
-                "assignment.h",
-            ],
-            "5f8539a4229b1b0006f11483",
-            10,
-        ],
-    ],
+        [["ex1.m"], "6343c517b6a0b300081b4d34", 1],
+        [["ex3.m"], "6343c54db6a0b300081b4d37", 1],
+        [["ex5.m"], "6343c570b6a0b300081b4d3a", 10],
+    ]
 }
-MANDATORY_FILES = []
-OPTIONAL_FILES = [
-    "ex3.c",
-    "ex4.c",
-    "ex1.h",
-    "ex2.h",
-    "ex3.h",
-    "ex4.h",
-    "ex5.h",
-    "main.c",
+
+MANDATORY_FILES = [
+    f"ex{i}.m" for i in [1, 3, 5]
 ]
+
+OPTIONAL_FILES = [
+    f"ex{i}.m" for i in [2, 4, 6]
+]
+
 PROJ_JOJ_INFO = {
     2: {
         "homeworkID": "5facbf7c9fedcc000661ff9d",
